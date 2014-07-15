941804aaef88147bbc27dc79e2b0e3e3651c91be
David Jones <drj@pobox.com>
2014-07-15 10:56:51 +0100

    Bigger scale on y-axis

--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for a in $(cat all)
 do
-  ~/ghcntool/stationplot.py -c 'ytick=2;legend=none' -y -d ~/zontem/input/ghcnm.v3.2.2.20140611/ghcnm.tavg.v3.2.2.20140611.qca.dat $a
+  ~/ghcntool/stationplot.py -c yscale=30 -c ytick=1 -c legend=none -y -d ~/zontem/input/ghcnm.v3.2.2.20140611/ghcnm.tavg.v3.2.2.20140611.qca.dat $a
   echo $a
 done
